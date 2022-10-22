72caee21f08da417f163b4aa2d0bb608cd7df00e
PolRubio <aporcrb23@gmail.com>
2022-10-22 15:26:08 +0200

    sobrecarrega constructor getTipusBrossa reitraViaPublica getUbicacio setUbicacio getCodi getEstat
    TODO: sobrecarrega operadors

--- ContenidorBrossa.cpp
+++ ContenidorBrossa.cpp
@@ -1,4 +1,6 @@
 #include <string>
+#include <regex> //legal??
+#include <time.h>
 
 using namespace std;
 
@@ -10,16 +12,30 @@ class ContenidorBrossa {
             string ubicacio,
             int anyColocacio, 
             float tara
-        ){
-            this->codi=codi;
-            this->color=color;
-            this->ubicacio=ubicacio;
-            this->anyColocacio=anyColocacio;
-            this->tara=tara;
-        }
+        ){ 
+            if (regex_match(codi, regex("[a-zA-Z]{2}-\d{4}"))) this->codi = codi;
+            else throw "El codi ha de tenir 2 lletres i 4 numeros";
 
-        // sobrecarregar el s’ubica el contenidor directament al magatzem de l’Ajuntament,  no  s’ubica  al  carrer
-        // si no es pot invocar l'altre, crear un amb valor per defecte var=Something
+            if (color >= 0 && color <= 4) this->color = color;
+            else throw "El color ha de ser un valor entre 0 i 4";
+
+            this->ubicacio=ubicacio;
+            
+            if (anyColocacio <= getAnyActual()) this->anyColocacio = anyColocacio;
+            else throw "No es pot colocar un contenidor en el futur";
+
+            if (tara >= 0) this->tara = tara;
+            else throw "La tara ha de ser un valor positiu";
+        }
+        ContenidorBrossa(
+            string codi, 
+            int color, 
+            int anyRetirada,
+            float tara
+        ){
+            this(codi, color, NULL, 0, tara);
+            this->anyRetirada=anyRetirada;
+        }
 
         const int GROC=0;
         const int GRIS=1;
@@ -27,7 +43,65 @@ class ContenidorBrossa {
         const int VERD=3;
         const int BLAU=4;
 
+        string getTipus(){
+            switch(color){
+                case GROC:
+                    return "Groc";
+                case GRIS:
+                    return "Gris";
+                case MARRO:
+                    return "Marro";
+                case VERD:
+                    return "Verd";
+                case BLAU:
+                    return "Blau";
+                default:
+                    return "Desconegut";
+            }
+        }
+        string getUbicacio(){
+            if (ubicacio == NULL) throw "El contenidor no esta ubicat";
+            else return ubicacio;
+        }
 
+        int getCodi(){
+            return (int) codi.substr(2); //aa-1234 -> 1234
+        }
+
+        string getEstat(){
+            if (ubicacio == NULL) return "Retirat";
+            if (getAnyActual() - anyColocacio > 5) return "Vell";
+            if (getAnyActual() - anyColocacio < 3) return "Nou";
+            return "Seminou";
+        }
+
+        void retirarViaPublica(){
+            if (ubicacio == NULL) throw "El contenidor no esta a la via pública";
+            this->anyRetirada=getAnyActual();
+            this->ubicacio=NULL;
+            this->anyColocacio=0;
+        }
+
+        void setUbicacio(string ubicacio){
+            if (ubicacio == NULL) retirarViaPublica();
+            else {
+                this->ubicacio=ubicacio;
+                this->anyColocacio=getAnyActual();
+            }
+        }
+
+        //TODO: Obligatòriament en la implementació d’un d’aquest operadors s’ha d’invocar als altres dos, i a més usant directament la simbologia de l’operador
+        bool operator==(ContenidorBrossa *p){
+            return codi.compare(p.codi);
+        }
+
+        bool operator<(ContenidorBrossa *p){
+            return this->getCodi() < p.getCodi();
+        }
+
+        bool operator>(ContenidorBrossa *p){
+            return this->getCodi() > p.getCodi();
+        }
 
     protected:
         int tara;
@@ -37,4 +111,13 @@ class ContenidorBrossa {
     string ubicacio;
     int anyColocacio;
     int anyRetirada;
+
+    private:
+        int getAnyActual(){
+            time_t now;
+            struct tm *now_tm;
+            now = time(NULL);
+            now_tm = localtime(&now);
+            return now_tm->tm_year + 1900;
+        }
 };
